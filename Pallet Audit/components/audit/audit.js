9cf63aae27060b8cdd4f72e603030f1f76a96c38
Devon O'Shaughnessy <devono@ulfoods.com>
2016-11-14 18:59:47 +0000

    Added validation to the audit screen.

--- Pallet Audit/components/audit/audit.js
+++ Pallet Audit/components/audit/audit.js
@@ -4,10 +4,10 @@
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
 // END_CUSTOM_CODE_home
-(function(pallet, $){
+(function (pallet, $) {
     var audit,
         app = pallet.app = pallet.app || {};
-    
+
     app.auditModel = new kendo.data.ObservableObject({
         PALLET_ID: '',
         EMP_NAME: '',
@@ -28,7 +28,7 @@
         ICE_CREAM: false
     });
     app.audit = {
-        onShow: function() {
+        onShow: function () {
             //binds the model above to the inputs in the form
             kendo.bind($("#palletForm"), app.auditModel);
 
@@ -37,22 +37,24 @@
 
             palletJSDO.subscribe('afterFill', onAfterFill);
             palletJSDO.fill();
-            
-            $("#btn-submit").on('click', function(){
+
+            $("#btn-submit").on('click', function () {
                 app.audit.submitAudit();
             });
-            $("#btn-cancel").on('click', function() {
+            $("#btn-cancel").on('click', function () {
                 app.audit.clearFields();
             });
 
             //Trying to make enter go to next input, must be tested yet
-            $("input").on("keyup", function(e) {
+            $("input").on("keyup", function (e) {
                 if (e.keycode == 13) {
                     $(this).next("input").focus();
                 }
             });
         },
-        setDateAndTime: function() {
+        validateData: function () {
+        },
+        setDateAndTime: function () {
             //Set the date and time in the model
             var date = app.audit.getDate();
             app.auditModel.set("STAMP_DT", date);
@@ -60,34 +62,41 @@
             var time = app.audit.getTime();
             app.auditModel.set("STAMP_TM", time);
         },
-        submitAudit: function(){
+        submitAudit: function () {
             var model = app.auditModel,
                 palletJSDO = app.palletAuditJSDO;
 
             app.audit.setDateAndTime();
-            	
-            palletJSDO.create(model);
-            palletJSDO.saveChanges();
 
-            alert("Audit submitted successfully!");
-            app.audit.clearFields();
+            var validator = $("#palletForm").kendoValidator().data("kendoValidator");
+
+            if (validator.validate()) {
+                palletJSDO.create(model);
+                palletJSDO.saveChanges();
+
+                alert("Audit submitted successfully!");
+                app.audit.clearFields();
+            }
+            else {
+                alert("Please complete the form.");
+            }
         },
-        clearFields: function() {
+        clearFields: function () {
             var model = app.auditModel;
-            
+
             for (var field in model) {
                 var value = model.get(field);
-                if (typeof(value) === "boolean"){
-                	model.set(field, false);
+                if (typeof (value) === "boolean") {
+                    model.set(field, false);
                 }
-                else if (typeof(value) === "string"){
-                    if((field != "STAMP_DT") && (field != "STAMP_TM")) {
+                else if (typeof (value) === "string") {
+                    if ((field != "STAMP_DT") && (field != "STAMP_TM")) {
                         model.set(field, '');
                     }
                 }
             }
         },
-        getReportId: function(jsdo, success, request) {
+        getReportId: function (jsdo, success, request) {
             var onAfterFill = app.audit.getReportId;
 
             jsdo.unsubscribe('afterFill', onAfterFill);
@@ -96,19 +105,19 @@
             var currentAuditID = parseInt(lastAuditID) + 1;
             app.auditModel.set("PALLET_ID", currentAuditID);
         },
-        getDate: function() {            
+        getDate: function () {
             var currentDate = new Date();
             var dateString = kendo.toString(currentDate, "yyyy-MM-dd");
-        	return dateString;
+            return dateString;
         },
-        getTime: function() {
+        getTime: function () {
             var currentDate = new Date(),
-            	dateString = currentDate.toString();
-        	var currentTime = dateString.substring(16, 21);
+                dateString = currentDate.toString();
+            var currentTime = dateString.substring(16, 21);
 
-        	return currentTime;
+            return currentTime;
         }
     }
-})(window,jQuery);
+})(window, jQuery);
 
 // END_CUSTOM_CODE_homeModel
